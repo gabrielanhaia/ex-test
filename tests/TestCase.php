05c2f6ce0f016c353b6692ad91281be0d0569231
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-03-02 16:44:31 +0000

    feature: Create TestCase class

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Tests;
+
+/**
+ * Class TestCase
+ * @package App\Tests
+ *
+ * @author Gabriel Anhaia <gabriel@stargrid.pro>
+ */
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
